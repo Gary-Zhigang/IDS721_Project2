79d4273bb361765534c1a1427e4f4bc46a66d222
zhigangwei <zhiw803@gmail.com>
2023-02-12 22:57:57 -0500

    init

--- app.py
+++ app.py
@@ -1,72 +1,33 @@
-"""Helllo World!
-Python program using Flask for a simple Calculator
-GUI using the flask module
-"""
-
-# import Flask Library
-
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, jsonify
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 
+# Spotify API credentials
+client_id = '427c7e94df2b4d1f89ad703f2d83d005'
+client_secret = '02a04d31f6a64e3ca79f8d16b7dfa51e'
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    # return '<h1>Hi, Welcome to this session</h1>'
 
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']
+    result = sp.search(query, limit=1, type='track')
 
-@app.route('/send', methods=['POST'])
-def send():
-    if request.method == 'POST':
-        x = request.form['x']
-        # print(x)
-        # if x is blank or
-        if x == "":
-            pass
-        else:
-            try:
-                x = int(x)
-            except:
-                return render_template('index.html', results="Non string value passed to x\n Please Provide an integer value")
-        expression = request.form['expression']
-        try:
-            results = eval(expression)
-            print(results)
-        except Exception as e:
-            message = """Oops! 
-            Your expression syntax is incorrect!
-            The full returned Traceback:
-            """+str(e)
-            return render_template('index.html', results=message)
-        return render_template('index.html', results=results)
+    if not result['tracks']['items']:
+        return jsonify({'message': 'No search results found.'}), 404
 
+    track_id = result['tracks']['items'][0]['id']
+    track_name = result['tracks']['items'][0]['name']
+    artist_name = result['tracks']['items'][0]['artists'][0]['name']
+    results = sp.recommendations(seed_tracks=[track_id], limit=10)
 
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    print("I am inside hello world")
-    return 'Hello World! I can make change at route: /change'
-
-@app.route('/change/<dollar>/<cents>')
-def changeroute(dollar, cents):
-    print(f"Make Change for {dollar}.{cents}")
-    amount = f"{dollar}.{cents}"
-    result = change(float(amount))
-    return jsonify(result)
-    
-    
-@app.route('/100/change/<dollar>/<cents>')
-def change100route(dollar, cents):
-    print(f"Make Change for {dollar}.{cents}")
-    amount = f"{dollar}.{cents}"
-    amount100 = float(amount) * 100
-    print(f"This is the {amount} X 100")
-    result = change(amount100)
-    return jsonify(result)
-
+    return render_template('results.html', query=query, track_name=track_name, artist_name=artist_name, results=results['tracks'])
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3000, debug=True)
+    app.run(port=8080, debug=True)
--- test_app.py
+++ test_app.py
@@ -1,5 +1,21 @@
-from app import change
+import unittest
+from app import app
 
+class TestApp(unittest.TestCase):
 
-def test_change():
-    assert [{5: 'quarters'}, {1: 'nickels'}, {4: 'pennies'}] == change(1.34)
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>Welcome to the Song Recommender</h1>', response.data)
+
+    def test_recommendations(self):
+        response = self.app.post('/recommendations', data=dict(song='Stay'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h2>Here are 10 songs similar to Stay:</h2>', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
